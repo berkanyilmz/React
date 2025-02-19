e092db9f69413b1a9a4c0482cf72c18ed5bf04ef
Berkan YILMAZ <bergan1905@gmail.com>
2025-02-19 15:03:21 +0300

    TODO List

---
+++ task/src/components/TaskShow.js
@@ -0,0 +1,42 @@
+import {useState} from "react";
+import TaskCreate from "./TaskCreate";
+
+function TaskShow( { task, onDelete, onUpdate } ) {
+
+    const [show, setShow] = useState(false);
+    
+    const handleDeleteClick = () => {
+        onDelete(task.id);
+    }
+
+    const handleEditClick = () => {
+        setShow(!show);
+    }
+
+    const handleSubmit = (id, updatedTitle, updatedTaskDesc) => {
+        setShow(false);
+        onUpdate(id, updatedTitle, updatedTaskDesc)
+    }
+
+    return <div className={'task-show'}>
+
+        {
+            show ?
+                <TaskCreate task={task} isUpdate={true} onUpdate={handleSubmit}/>
+                : <div>
+                    <h3 className={'task-title'}>Göreviniz</h3>
+                    <p>{ task.title }</p>
+                    <h3 className={'task-title'}>Yapılacaklar</h3>
+                    <p> { task.taskDesc } </p>
+                    <div>
+                        <button className={'task-delete'} onClick={handleDeleteClick}>Sil</button>
+                        <button className={'task-update'} onClick={handleEditClick}>Güncelle</button>
+                    </div>
+                </div>
+        }
+
+
+    </div>
+}
+
+export default TaskShow;
